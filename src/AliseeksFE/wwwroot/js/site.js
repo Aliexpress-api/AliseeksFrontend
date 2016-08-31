ab7d7342ee4622886a38a668c1096ab4eb0b7467
Alex Bello <abello.2015@gmail.com>
2016-08-31 18:12:17 -0500

    Reorganized SASS files for better design

--- src/AliseeksFE/wwwroot/js/site.js
+++ src/AliseeksFE/wwwroot/js/site.js
@@ -26,31 +26,3 @@ function premptiveSearchDoneTyping() {
         }
     });
 }
-$(premptiveSearchCache);
-
-const doneTypingDelay = 200;
-var typingTimer;
-var ajaxCache = "/search/cache";
-
-function premptiveSearchCache() {
-    $('#search-text').on('input', function () {
-        clearTimeout(typingTimer);
-        typingTimer = window.setTimeout(premptiveSearchDoneTyping, doneTypingDelay);
-    });
-    $('#searchForm').on('submit', function () {
-        clearTimeout(typingTimer);
-    });
-}
-
-function premptiveSearchDoneTyping() {
-    console.log("Triggered done typing event");
-    let form = $('#searchForm');
-    let endpoint = ajaxCache + '?' + form.serialize();
-    console.log("Sending ajax to: " + endpoint);
-    $.ajax(endpoint, {
-        method: "get",
-        success: function () {
-            console.log("Successful response on ajax");
-        }
-    });
-}
