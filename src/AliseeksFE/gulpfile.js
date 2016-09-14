e901c4d360b6c8598940a81a4343b07edbb4d618
Alex Bello <abello.2015@gmail.com>
2016-09-14 16:21:01 -0500

    Fixed mobile height display issue

--- src/AliseeksFE/gulpfile.js
+++ src/AliseeksFE/gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
     concat = require("gulp-concat"),
     uglify = require("gulp-uglify"),
     sass = require("gulp-sass"),
-    watch = require("gulp-watch"),
+    watch = require("gulp-watch");
     uglifycss = require('gulp-uglifycss'),
     pump = require('pump');
 
