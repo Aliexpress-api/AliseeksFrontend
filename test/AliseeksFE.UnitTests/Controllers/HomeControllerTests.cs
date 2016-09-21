f3571198572d8c5566a65b27ab16bbcb7fcc09f4
Alex Bello <abello.2015@gmail.com>
2016-09-21 14:24:00 -0500

    Add unit tests

---
+++ test/AliseeksFE.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Xunit;
+using System.Net.Http;
+using AliseeksFE;
+using System.IO;
+
+namespace AliseeksFE.UnitTests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public HomeControllerTests()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>());
+
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task CanReachIndex()
+        {
+            var response = await _client.GetAsync(Endpoints.Index);
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
---
+++ test/AliseeksFE.UnitTests/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliseeksFE.UnitTests
+{
+    public class Endpoints
+    {
+        public const string Index = "/";
+    }
+}
