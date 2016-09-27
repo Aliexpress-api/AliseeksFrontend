4d50b02641378a93ca4a8ea88ea6986e4220325e
Alex Bello <abello.2015@gmail.com>
2016-09-26 21:19:21 -0500

    Added quantity filtering

--- src/AliseeksFE/wwwroot/_references.js
+++ src/AliseeksFE/wwwroot/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="dev/js/search/search.js" />
+/// <reference path="js/site.js" />
 /// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
